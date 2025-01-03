4d8c658b9200a92f80ff5876dc4a77d538e8f526
ali hamzayev <008461@ibar.int>
2025-01-03 16:46:56 +0400

    solve binary tree inorder traversal by myself.

---
+++ src/main/java/easy/BinaryTreeInorderTraversal.java
@@ -0,0 +1,56 @@
+package easy;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
+
+public class BinaryTreeInorderTraversal {
+    public static void main(String[] args) {
+
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new LinkedList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        stack.add(root);
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.peek();
+            if (node == null) {
+                stack.pop();
+            } else {
+                TreeNode left = node.left;
+                TreeNode right = node.right;
+                if (left != null) {
+                    stack.push(left);
+                    node.left = null;
+                } else if (right == null) {
+                    result.add(stack.pop().val);
+                } else {
+                    result.add(stack.pop().val);
+                    stack.push(right);
+                }
+            }
+        }
+        return result;
+    }
+}
